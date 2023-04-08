901c84a541f6336a75bd7d2f138a03658c128216
Sunny sah <80568603+Sunnsah@users.noreply.github.com>
2023-04-08 12:32:26 +0545

    Add files via upload
    Non-Credit Course by Saroj shakya

---
+++ Full Stack/SocialMediaThemeKMC/js/index.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName('test')[0].addEventListener("click", function() {
+    alert("Test is clicked");
+})
+
+document.getElementsByName('search')[0].addEventListener("mouseover", function() {
+    alert("Hover");
+})
